205d6f7dc378b4a77b77cbc67172f524b74df13b
Albert Martin <albert@imor.tl>
2017-05-28 22:46:39 -0500

    chore(build): set random hash on each build

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,17 @@ const webpack = require('webpack');
 
 // Plugins being used in the webpack build process.
 const CleanPlugin = require('clean-webpack-plugin');
+const { WebPlugin } = require('web-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 let config = {
   devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'eval-source-map',
-  entry: ['normalize.css/normalize.css', './src/main.js', './src/styles/index.scss'],
+  entry: { app: ['normalize.css/normalize.css', './src/main.js', './src/styles/index.scss'] },
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/',
-    filename: 'app.js',
+    filename: '[name].[hash].js',
   },
   module: {
     rules: [
@@ -44,6 +45,11 @@ let config = {
         NODE_ENV: `"${process.env.NODE_ENV}"`,
       },
     }),
+    new WebPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      requires: ['app'],
+    }),
   ],
   resolve: {
     alias: {
@@ -56,7 +62,7 @@ let config = {
 
 if (process.env.NODE_ENV === 'production') {
   config.plugins = config.plugins.concat([
-    new CleanPlugin(['dist'], { exclude: ['index.html'] }),
+    new CleanPlugin(['dist']),
     new webpack.optimize.UglifyJsPlugin({
       output: {
         comments: false,
